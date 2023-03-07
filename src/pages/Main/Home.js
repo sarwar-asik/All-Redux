f9f0fe5dd8605c1384591f1de8e914372da034c6
sarwar-asik <shasik2021@gmail.com>
2023-03-07 16:27:51 +0600

    fetch products

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../../components/ProductCard";
 import { toggleBrand, toggleStock } from "../../redux/actions/filterActions";
@@ -9,20 +9,26 @@ const Home = () => {
   // const products = useSelector((state) => state.product.products);
   // const { brands, stock } = filters;
   const dispatch = useDispatch();
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    dispatch(loadProductData());
-  }, [dispatch]);
+    // dispatch(loadProductData());
+
+    fetch(`http://localhost:5000/products`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data.data));
+  }, []);
 
   const activeClass = "text-white bg-indigo-500 border-white";
 
   let content;
 
-  // if (products.length) {
-  //   content = products?.map((product) => (
-  //     <ProductCard key={product.model} product={product} />
-  //   ));
-  // }
+  if (products.length) {
+    content = products?.map((product) => (
+      <ProductCard key={product.model} product={product} />
+    ));
+  }
+  console.log(content,"=======================");
 
   // if (products.length && (stock || brands.length)) {
   //   content = products
@@ -42,8 +48,8 @@ const Home = () => {
   // }
 
   return (
-    <div className='max-w-7xl gap-14 mx-auto my-10'>
-      <div className='mb-10 flex justify-end gap-5'>
+    <div className="max-w-7xl gap-14 mx-auto my-10">
+      <div className="mb-10 flex justify-end gap-5">
         <button
           onClick={() => dispatch(toggleStock())}
           className={`border px-3 py-2 rounded-full font-semibold  `}
@@ -63,7 +69,7 @@ const Home = () => {
           Intel
         </button>
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14'>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14">
         {content}
       </div>
     </div>
