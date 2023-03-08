96de9cdb139b59941c8483b1cdc9b6ecf5f6cbe7
sarwar-asik <shasik2021@gmail.com>
2023-03-08 23:12:29 +0600

    logger

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore} from "@reduxjs/toolkit";
 import cartSlice from "../feature/cart/cartSlice";
 import filterSlice from "../feature/filter/filterSlice";
+import logger from "redux-logger";
 
 const store = configureStore({
-    reducer:{
-        cart:cartSlice,
-        filter:filterSlice
-    }
-})
+  reducer: {
+    cart: cartSlice,
+    filter: filterSlice,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
 
-
-export default store
+export default store;
