ffc2bca559baf5031ccea872b4648e55ecc27d03
sarwar-asik <shasik2021@gmail.com>
2023-03-13 16:16:56 +0600

    apiSlice

---
+++ src/feature/api/apiSlice.js
@@ -0,0 +1,13 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+
+const productApi = createApi({
+    reducerPath:"productApi",
+    baseQuery:fetchBaseQuery({
+        baseUrl:"http://localhost:5000"
+    }),
+    endpoints:(builder)=>({
+        getProducts: builder.query({
+            query:()=>"/products"
+        })
+    })
+})
