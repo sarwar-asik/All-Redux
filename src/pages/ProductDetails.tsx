103702839892810661a52022bf25834bfd0af393
sarwar-asik <sarwarasik@gmail.com>
2023-07-10 12:22:51 +0600

    rtk query product

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,21 +1,43 @@
 import ProductReview from '@/components/ProductReview';
 import { Button } from '@/components/ui/button';
+import { toast } from '@/components/ui/use-toast';
+import { useGetSingleProductQuery } from '@/redux/api/apiSlice';
+import { addToCart } from '@/redux/features/cart/cartSlice';
+import { useAppDispatch } from '@/redux/hooks';
 import { IProduct } from '@/types/globalTypes';
-import { useEffect, useState } from 'react';
+import {
+  JSXElementConstructor,
+  Key,
+  ReactElement,
+  ReactFragment,
+  useEffect,
+  useState,
+} from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function ProductDetails() {
   const { id } = useParams();
+  const dispatch = useAppDispatch();
 
   //! Temporary code, should be replaced with redux
-  const [data, setData] = useState<IProduct[]>([]);
-  useEffect(() => {
-    fetch('../../public/data.json')
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
+  // const [data, setData] = useState<IProduct[]>([]);
+  // useEffect(() => {
+  //   fetch('../../public/data.json')
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data));
+  // }, []);
 
-  const product = data?.find((item) => item._id === Number(id));
+  const { data: product } = useGetSingleProductQuery(id);
+
+  const handleAddProduct = (productData: IProduct) => {
+    dispatch(addToCart(productData));
+
+    toast({
+      description: 'Product Added',
+    });
+  };
+
+  // const product = data?.find((item) => item._id === Number(id));
 
   //! Temporary code ends here
 
@@ -29,11 +51,21 @@ export default function ProductDetails() {
           <h1 className="text-3xl font-semibold">{product?.name}</h1>
           <p className="text-xl">Rating: {product?.rating}</p>
           <ul className="space-y-1 text-lg">
-            {product?.features?.map((feature) => (
-              <li key={feature}>{feature}</li>
-            ))}
+            {product?.features?.map(
+              (
+                feature:
+                  | boolean
+                  | Key
+                  | ReactElement<any, string | JSXElementConstructor<any>>
+                  | ReactFragment
+                  | null
+                  | undefined
+              ) => (
+                <li key={feature}>{feature} </li>
+              )
+            )}
           </ul>
-          <Button>Add to cart</Button>
+          <Button onClick={() => handleAddProduct(product)}>Add to cart</Button>
         </div>
       </div>
       <ProductReview />
--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,14 +1,19 @@
-import { IProduct } from "@/types/globalTypes"
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+import { IProduct } from '@/types/globalTypes';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
-    reducerPath:"api",
-    baseQuery:fetchBaseQuery({baseUrl:'http://localhost:5000'}),
-    endpoints:(builder)=>({
-        getProducts:builder.query({
-            query:()=>'/products'
-        })
-    })
-})
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://technet-server-sigma.vercel.app',
+  }),
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => '/products',
+    }),
+    getSingleProduct: builder.query({
+      query: (id) => `/product/${id}`,
+    }),
+  }),
+});
 
-export const {useGetProductsQuery} = api
+export const { useGetProductsQuery ,useGetSingleProductQuery} = api;
